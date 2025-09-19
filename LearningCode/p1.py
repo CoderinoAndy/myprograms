d5ea42462fedc09081e10bfb30ffb5df3b7cf5b8
CoderinoAndy <burritoman801@gmail.com>
2025-09-19 10:36:38 -0400

    Newly commented

--- LearningCode/p1.py
+++ LearningCode/p1.py
@@ -1,3 +1,12 @@
-name = input("Name: ")
-age = int(input("Age: "))
-print(f"Your name is {name} and you are {age} years old!")
+# My first program
+# Created on 09/15/2025
+# Purpose: to learn python
+# Editor: Andy Yu
+
+name = input("Hey there, can I have your name?: ")
+age = input(f"Hey there {name}, can I have your age?: ")
+
+print(f"What's up {name}, you look old for a {age} year old! I'm just joking.")
+
+# "input()" is a function which takes keyboard input in the console.
+# variable "name" and variable "age" is a labelled container of pesistent data.
